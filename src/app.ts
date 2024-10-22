92a15d034321611c0617a55ab617c7585fa125c2
kou7306 <kouta.1206.y@icloud.com>
2024-10-22 20:20:52 +0900

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import http from "http";
+import cors from "cors"; // corsをインポート
 import prisma from "./config/prisma";
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-
+app.use(express.json());
 // CORSの設定
 const corsOptions = {
-  origin: "*",
-  methods: ["*"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
+  origin: "*", // 全てのオリジンを許可
+  methods: ["GET", "POST", "PUT", "DELETE"], // 許可するメソッド
+  allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"], // 許可するヘッダー
 };
+
+// CORSミドルウェアを使用
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -21,8 +25,13 @@ app.get("/", (req, res) => {
 
 // ユーザーの取得
 app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export { app, server };
